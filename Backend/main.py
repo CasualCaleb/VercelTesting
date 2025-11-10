7b007f69ed5cb2952d3b8d7aa88fd1c189c08d82
casualcaleb <cookiecaleb@gmail.com>
2025-11-10 01:17:17 -0800

    Added basic backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,8 @@
 from app import create_app
+import os
 
 if __name__ == '__main__':
     client = create_app()
-    client.run()
+    port = int(os.environ.get("PORT", 5000))
+
+    client.run(host="0.0.0.0", port=port)
